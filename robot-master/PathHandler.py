89ae91f6df43fa7ac09983f3aaf12ef66142ab82
LaithRuweha <55024691+LaithRuweha@users.noreply.github.com>
2019-09-19 13:55:22 +0200

    Add files via upload

---
+++ robot-master/PathHandler.py
@@ -0,0 +1,30 @@
+from math import pow, sqrt
+
+class PathHandler:
+
+
+    def getNextPathPoint ( self, position, path, startPoint, lookaheadDistance ):
+       
+       ### set variable
+        currentPoint = startPoint
+        currentDistance = 0
+       
+        while ( currentDistance < lookaheadDistance ):
+            currentDistance = self.calculateDistance(position['X'], position['Y'], path[currentPoint]['X'], path[currentPoint]['Y'])
+            currentPoint = currentPoint + 1
+            if(self.isLastPoint(path, currentPoint)):
+                currentDistance = 999999
+
+        return ( currentPoint - 1 )
+    
+    def isLastPoint ( self, path, currentPoint):
+        if ( currentPoint == (len(path) - 1)):
+            return True
+        
+        return ( False )
+
+
+    def calculateDistance ( self, xHome, yHome, xAway, yAway ):
+            distance = sqrt(pow(xAway - xHome, 2) + pow(yAway - yHome, 2))
+            return ( distance )
+
---
+++ robot-master/TestRobot3.py
@@ -0,0 +1,56 @@
+from Robot import Robot
+from time import sleep
+from Path import Path
+from Calibrate import Calibrate
+from AngleConverter import AngleConverter
+from PathHandler import PathHandler
+from Navigator import Navigator
+from Goal import Goal
+
+# load a path file
+#p = Path("Path-around-table.json")
+p = Path("Path-around-table-and-back.json")
+#p = Path("Path-from-bed.json")
+#p = Path("Path-to-bed.json")
+path = p.getPath()
+
+print("Path length = " + str(len(path)))
+print("First point = " + str(path[0]['X']) + ", " + str(path[0]['Y']))
+
+# make a robot to move around
+robot = Robot()
+converter = AngleConverter()
+pathHandler = PathHandler()
+navigator = Navigator()
+goal = Goal()
+
+#### Intialize variables
+position = robot.getPosition()
+speed = 0.25
+heading = 0
+nextPoint = 0
+dropOut = 0
+lookAheadDistance = 0.85
+
+while ( goal.notGoal( position, nextPoint, path ) ):
+
+    ### get current status (position, heading)
+    position = robot.getPosition()
+    heading = converter.convertToDegree(robot.getHeading())
+    
+    ### get next point
+    nextPoint = pathHandler.getNextPathPoint( position, path, nextPoint, lookAheadDistance)
+    print(nextPoint)
+
+    ### find direction to path
+    directionToPoint = navigator.getDirection( position, path, nextPoint )    
+    turnRate = navigator.getTurnRate( heading, directionToPoint )
+
+    ### set motion
+    robot.setMotion(speed, turnRate )
+    sleep(0.35)
+    robot.setMotion(0,0 )
+
+
+print('Geeeeeehaaaaaaa')
+
